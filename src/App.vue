23f787dc78effbb9eca4e074cd5551031b9b27df
Samuel Burbano <samuel@hipecommerce.com>
2022-04-12 20:34:38 -0500

    feat: default slots content and slot props

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-  <select v-model="currentLocale">
-    <option value="en">English</option>
-    <option value="es">Español</option>
-  </select>
-  <hr />
-  <i18n tag="h2" path="hello">
-    {{ name }}
-  </i18n>
-  <i18n tag="h3" path="welcome">
-    <template #name>
-      {{ name }}
+  <default-layout>
+    <template #header>
+      <h1>Slot tutorial</h1>
     </template>
-    <template #app>
-      {{ app }}
+
+    <template #default="{ color }">
+      <select v-model="currentLocale">
+        <option value="en">English</option>
+        <option value="es">Español</option>
+      </select>
+      <hr />
+      <i18n tag="h2" path="hello">
+        {{ name }}
+      </i18n>
+      <i18n tag="h3" path="welcome">
+        <template #name>
+          {{ name }}
+        </template>
+        <template #app>
+          {{ app }}
+        </template>
+      </i18n>
+      <input :style="`background-color: ${color}`" type="text" v-model="name" />
     </template>
-  </i18n>
-  <input type="text" v-model="name" />
+
+    <template #footer="{ year }">
+      <p>Slots &copy; {{ year }}</p>
+    </template>
+  </default-layout>
 </template>
 
 <script setup>
+import defaultLayout from "./layouts/default.vue";
 import { inject, ref } from "vue";
 
 const currentLocale = inject("currentLocale");
---
+++ src/layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <slot name="header">
+      <h1>Welcome to my site</h1>
+    </slot>
+  </header>
+  <main>
+    <div class="wrapper">
+      <slot color="#697d7f" />
+    </div>
+  </main>
+  <footer>
+    <slot name="footer" :year="year">
+      <p>&copy; {{ year }}</p>
+    </slot>
+  </footer>
+</template>
+
+<script setup>
+const year = new Date().getFullYear();
+</script>
+
+<style>
+header {
+  background: #333;
+  color: #fff;
+  padding: 1rem;
+}
+
+main .wrapper {
+  padding: 1rem;
+}
+
+footer {
+  background: #333;
+  color: #fff;
+  padding: 1rem;
+}
+</style>
